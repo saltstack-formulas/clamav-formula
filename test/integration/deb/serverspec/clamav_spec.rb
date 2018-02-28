1a41fe4c6b55c3c10c26ac4fa6afba6e93250f0a
Niels Abspoel <aboe76@gmail.com>
2018-02-28 20:07:10 +0100

    fix specs

--- test/integration/deb/serverspec/clamav_spec.rb
+++ test/integration/deb/serverspec/clamav_spec.rb
@@ -6,18 +6,6 @@ describe package('clamav') do
   it { should be_installed }
 end
 
-describe 'clamav service' do
-
-  case os[:family]
-  when 'redhat'
-    service = 'freshclam'
-  when 'debian', 'ubuntu'
-    service = 'clamav-freshclam'
-  else
-    # No other supported ATM
-  end
-
-  describe service(service) do
+describe service('clamav-freshclam') do
   it { should be_enabled }
-  end
 end
